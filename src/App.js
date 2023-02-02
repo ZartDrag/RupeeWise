6f63280476f5e3c49d9070b543ea8fc1d287a94b
Niti-Chauhan-16 <121387728+Niti-Chauhan-16@users.noreply.github.com>
2023-02-02 19:30:37 +0530

    did some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import  './Mycomponentcss/Main.css';
+import Navbar from './Mycomponent/Navbar';
+import Header from './Mycomponent/Header';
+import AboutUs from './Mycomponent/AboutUs';
+import Features from './Mycomponent/Features';
+import Contact from './Mycomponent/Contact';
+import Footer from './Mycomponent/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Header/>
+    <AboutUs/>
+    <Features/>
+    <Contact/>
+    <Footer/>
+
+    </>
   );
 }
 
---
+++ src/Mycomponent/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div class="footer">
+      <div class="footer--container">
+        <div class="footer--bar">
+          <div class="footer--logo">
+            <p class="logo--text u-white"><span>₹</span>uppeWise</p>
+          </div>
+        </div>
+        <hr />
+        <div class="copyright__bar">
+          <div class="copyright__box">
+            <div class="copyright__text--left">
+              <p>©2023 ₹uppeWise. All rights reserved</p>
+            </div>
+            <div class="copyright__text--right">
+              <span class="material-symbols-outlined i-support">
+                support_agent
+              </span>
+              &nbsp;&nbsp;&nbsp;
+              <span class="contact--number"
+                >24/7 &nbsp;&nbsp;&nbsp;037-2339-9874</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
